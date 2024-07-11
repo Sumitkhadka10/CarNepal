c55f1f0b67893609a38e223f43b4e561cb28c947
Sarthak0071 <sarthak8618@gmail.com>
2024-07-11 12:10:46 +0545

    top navigation done

--- src/components/Client/Topnavigation.jsx
+++ src/components/Client/Topnavigation.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import './Topnavigation.css'
+
 function Topnavigation() {
   return (
-    <div>
-   
-      <h1>Janak Chor Ho ho </h1>
-  
+    <div className="top-nav-container">
+      <div className="top-left-section">
+        <div className="top-nav-container-logo">Logo</div>
+        <div className="top-nav-search-container">
+          <input type="text" className="top-nav-container-search-bar" placeholder="Search..." />
+          <button className="search-button">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+              <circle cx="11" cy="11" r="8"></circle>
+              <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
+            </svg>
+          </button>
+        </div>
+      </div>
+      <div className="top-nav-container-right-section">
+        <a href="#">Login</a>
+        <span>/</span>
+        <a href="#">Register</a>
+      </div>
     </div>
   );
 }
